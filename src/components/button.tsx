ff8258f76a38fd7c01ff006400e6f418d1f8500c
Aydin Terzi <aydinterzi7@gmail.com>
2023-08-30 20:18:37 +0300

    fix: color

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -15,12 +15,12 @@ function Button({ variant = "default", disableShadow, disabled, startIcon, endIc
   const btnDisabled = disabled ? 'btn-disabled' : ''
   const btnDisabledShadow = disableShadow ? '' : 'shadow-md'
   const btnSize = size === "sm" ? 'p-1' : size === "md" ? 'p-2' : 'p-3'
-  const btnColor = `bg-${color} hover:bg-${color}/70`
+  const btnColor = color === "default" ? 'bg-default text-dark' : color === "primary" ? 'bg-primary text-white' : color === "secondary" ? 'bg-secondary text-white' : 'bg-danger text-white'
   const btnVariant = variant === "default" ? '' : variant === "outline" ? `border border-${color} bg-white text-${color}` : `text-${color}`
   return (
     <>
       <button
-        className={`flex items-center gap-2  ${btnSize} rounded-lg ${btnColor} ${btnVariant} ${btnDisabledShadow} ${btnDisabled}`}
+        className={`flex items-center gap-2  ${btnSize} rounded-lg ${btnColor} ${btnVariant} ${btnDisabledShadow} ${btnDisabled} hover:bg-${color}/70`}
       >
         {startIcon}
         Button
